93ba182f201c12cca41f3c9426b605ed68f86cba
Taqi Enari <91334788+dbAbstract@users.noreply.github.com>
2022-05-15 15:28:15 +0900

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 extern crate serde_json;
-use rocket::*;
+use rocket::{*, http::Status};
 use rocket_contrib::json::Json;
 use rusqlite::Connection;
 use serde::Serialize;
@@ -27,11 +27,11 @@ struct StatusMessage {
 // Home page
 #[get("/")]
 fn index() -> &'static str {
-    "Welcome Taqi!"
+    "Welcome!"
 }
 
 
-// POST a number of items into DB endpoint
+// 1. POST a number of items into table endpoint
 #[post("/item/<item_id>/<quantity>/<table_num>")]
 fn add_item(item_id: i64, quantity:i64, table_num: i64) -> Result<Json<StatusMessage>, String> {
     let t = rand::thread_rng().gen_range(5..16);
@@ -73,12 +73,34 @@ fn add_item(item_id: i64, quantity:i64, table_num: i64) -> Result<Json<StatusMes
     }
 }
 
+// 2. DEL an item from a table endpoint
+#[delete("/item/<item_id>/<table_num>")]
+fn delete_item(item_id: i64, table_num: i64) -> Result<Json<StatusMessage>, String> {
+    let db_connection = match Connection::open("data.sqlite") {
+        Ok(connection) => connection,
+        Err(_) => {
+            return Err(String::from("Failed to connect to database"));
+        }
+    };
+    
+    let mut statement = match db_connection.prepare(&format!("delete from table_{} where item_id = {};", table_num, item_id)) {
+        Ok(statement) => statement,
+        Err(_) => return Err("Failed to prepare query".into()),
+    };
 
+    let results = statement.execute(rusqlite::NO_PARAMS);
 
-// DEL an item from a table endpoint
+    match results {
+        Ok(rows_affected) => Ok(Json(StatusMessage {
+            message: format!("{} row deleted!", rows_affected),
+        })),
+        Err(_) => Err("Failed to delete menu item".into()),
+    }
 
-// GET all items for a table endpoint
-#[get("/getAll/<table_num>")]
+}
+
+// 3. GET all items for a table endpoint
+#[get("/item/<table_num>")]
 fn get_all_items(table_num: i64) -> Result<Json<ItemList>, String> {
     
     // Connecting to database
@@ -123,11 +145,13 @@ fn get_all_items(table_num: i64) -> Result<Json<ItemList>, String> {
     }
 }
 
+// 4. GET specified item 
+
+// 5. UPDATE quantity of some specified item 
 
 
 
 
-// GET specified item 
 fn main() {
 
     let db_connection = Connection::open("data.sqlite").unwrap();
@@ -135,7 +159,7 @@ fn main() {
     rocket::ignite()
     .mount(
         "/",
-        routes![index, get_all_items, add_item]
+        routes![index, get_all_items, add_item, delete_item]
     )
     .launch();
 }
